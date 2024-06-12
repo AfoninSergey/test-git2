4a5a67afd58840481efe2275487b394dfedb187b
Gray Spaer <afoninsergey@mail.ru>
2024-06-12 20:54:27 +0300

    "Made changes in master"

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function sayHello(name = 'Gray') {
 	console.log(`Hello, ${name}!`);
 }
 sayHello('Gray Spaer');
+
+const userAge = 40
+
+if (age >= 18) {
+    console.log('Вы совершеннолетний')
+} else {
+    console.log('Вы НЕ совершеннолетний')
+}
