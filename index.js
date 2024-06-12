c16b88e6f5c41c7cc6fc41e0d49698151d835bc5
Gray Spaer <afoninsergey@mail.ru>
2024-06-12 20:45:08 +0300

    "Create and changes in feature/user-access"

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function sayHello(name = 'Gray') {
 	console.log(`Hello, ${name}!`);
 }
 sayHello('Gray Spaer');
+
+const age = 20;
+
+if (age >= 18) {
+    console.log("Вы совершеннолетний")
+} else {
+    console.log("Вы НЕ совершеннолетний")
+}
